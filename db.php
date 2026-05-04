38af7dcc36672f33b71eb876ce7fc16e5261998f
WhyMeLol891 <derricklimyeongwei@gmail.com>
2026-05-04 14:46:00 +0800

    restaurant_system

--- db.php
+++ db.php
@@ -1,29 +1,11 @@
 <?php
+ 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "restaurant_system";
 
-declare(strict_types=1);
-
-function getDbConnection(?string &$error = null): ?PDO
-{
-    $host = '127.0.0.1';
-    $port = 3306;
-    $dbName = 'restaurant_system';
-    $username = 'root';
-    $password = '';
-
-    $dsn = sprintf('mysql:host=%s;port=%d;dbname=%s;charset=utf8mb4', $host, $port, $dbName);
-
-    try {
-        return new PDO(
-            $dsn,
-            $username,
-            $password,
-            [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            ]
-        );
-    } catch (PDOException $exception) {
-        $error = $exception->getMessage();
-        return null;
-    }
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
